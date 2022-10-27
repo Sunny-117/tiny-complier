9f5c92732567a3f5f47a7b72b2916457dc2d6119
付志强 <zhiqiangfu6@gmail.com>
2022-10-27 13:45:31 +0800

    add new feature

--- tokenizer.ts
+++ tokenizer.ts
@@ -18,35 +18,35 @@ export function tokenizer(code: string) {
             value: char
         })
     }
-    if (char === '') ') {
-    tokens.push({
-        type: TokenTypes.Paren,
-        value: char
-    })
-}
-const LETTERS = /[a-z]/i
-if (LETTERS.test(char)) {
-    let value = ''
-    while (LETTERS.test(char) && current < code.length) {
-        value += char
-        char = code[++current];
+    if (char === ')') {
+        tokens.push({
+            type: TokenTypes.Paren,
+            value: char
+        })
     }
-    tokens.push({
-        type: TokenTypes.Name,
-        value
-    })
-}
-const NUMBERS = /[0-9]/
-if (NUMBERS.test(char)) {
-    let value = ''
-    while (NUMBERS.test(char) && current < code.length) {
-        value += char
-        char = code[++current];
+    const LETTERS = /[a-z]/i
+    if (LETTERS.test(char)) {
+        let value = ''
+        while (LETTERS.test(char) && current < code.length) {
+            value += char
+            char = code[++current];
+        }
+        tokens.push({
+            type: TokenTypes.Name,
+            value
+        })
     }
-    tokens.push({
-        type: TokenTypes.Number,
-        value
-    })
-}
-return tokens
+    const NUMBERS = /[0-9]/
+    if (NUMBERS.test(char)) {
+        let value = ''
+        while (NUMBERS.test(char) && current < code.length) {
+            value += char
+            char = code[++current];
+        }
+        tokens.push({
+            type: TokenTypes.Number,
+            value
+        })
+    }
+    return tokens
 }   
