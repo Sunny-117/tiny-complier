d92f795503a7bc0dc7ccd09eaecd3943f7a5afc2
付志强 <zhiqiangfu6@gmail.com>
2022-10-27 13:46:03 +0800

    add new feature

--- tokenizer.spec.ts
+++ tokenizer.spec.ts
@@ -1,20 +1,6 @@
 import { test, expect } from 'vitest'
 import { tokenizer, TokenTypes } from './tokenizer'
-test.skip('tokenizer', () => {
-    const code = `(add 2 (subtract 4 2))`
-    const tokens = [
-        { type: 'paren', value: '(' },
-        { type: 'name', value: 'add' },
-        { type: 'number', value: '2' },
-        { type: 'paren', value: '(' },
-        { type: 'name', value: 'subtract' },
-        { type: 'number', value: '4' },
-        { type: 'number', value: '2' },
-        { type: 'paren', value: ')' },
-        { type: 'paren', value: ')' },
-    ]
-    expect(tokenizer(code)).toEqual(tokens)
-})
+
 
 test('left paren', () => {
     const code = `(`
@@ -39,5 +25,21 @@ test('add', () => {
 test('number', () => {
     const code = `22`
     const tokens = [{ type: TokenTypes.Number, value: '22' }]
+    expect(tokenizer(code)).toEqual(tokens)
+})
+
+test('(add 1 1)', () => {
+    const code = `(add 1 1)`
+    const tokens = [
+        { type: 'paren', value: '(' },
+        { type: 'name', value: 'add' },
+        { type: 'number', value: '2' },
+        { type: 'paren', value: '(' },
+        { type: 'name', value: 'subtract' },
+        { type: 'number', value: '4' },
+        { type: 'number', value: '2' },
+        { type: 'paren', value: ')' },
+        { type: 'paren', value: ')' },
+    ]
     expect(tokenizer(code)).toEqual(tokens)
 })
