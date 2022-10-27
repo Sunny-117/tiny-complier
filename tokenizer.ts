04679d0439d16f496128a3ee47ca8bda4572d3a0
付志强 <zhiqiangfu6@gmail.com>
2022-10-27 15:30:00 +0800

    add new feature

--- tokenizer.ts
+++ tokenizer.ts
@@ -4,61 +4,72 @@ export enum TokenTypes {
     Number,
     String
 }
+
 export interface Token {
     type: TokenTypes;
     value: string;
-
 }
+
 export function tokenizer(code: string) {
-    const tokens: Token[] = []
+    const tokens: Token[] = [];
     let current = 0;
+
     while (current < code.length) {
         let char = code[current];
-        const WHITESPACE = /\s/
+
+        const WHITESPACE = /\s/;
         if (WHITESPACE.test(char)) {
-            current++
-            continue
+            current++;
+            continue;
         }
-        if (char === '(') {
+
+        if (char === "(") {
             tokens.push({
                 type: TokenTypes.Paren,
-                value: char
-            })
-            current++
-            continue
+                value: char,
+            });
+            current++;
+            continue;
         }
-        if (char === ')') {
+
+        if (char === ")") {
             tokens.push({
                 type: TokenTypes.Paren,
-                value: char
-            })
-            current++
-            continue
+                value: char,
+            });
+            current++;
+            continue;
         }
-        const LETTERS = /[a-z]/i
+
+        const LETTERS = /[a-z]/i;
         if (LETTERS.test(char)) {
-            let value = ''
+            let value = "";
             while (LETTERS.test(char) && current < code.length) {
-                value += char
+                value += char;
                 char = code[++current];
             }
+
             tokens.push({
                 type: TokenTypes.Name,
-                value
-            })
+                value,
+            });
         }
-        const NUMBERS = /[0-9]/
+
+        const NUMBERS = /[0-9]/;
+
         if (NUMBERS.test(char)) {
-            let value = ''
+            let value = "";
             while (NUMBERS.test(char) && current < code.length) {
-                value += char
+                value += char;
                 char = code[++current];
             }
+
             tokens.push({
                 type: TokenTypes.Number,
-                value
-            })
+                value,
+            });
         }
     }
-    return tokens
-}   
+
+    return tokens;
+}
