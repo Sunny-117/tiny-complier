4da316dc307353055414913e2bf15c00690acea5
付志强 <zhiqiangfu6@gmail.com>
2022-10-27 15:10:40 +0800

    add new feature

---
+++ transformer.spec.ts
@@ -0,0 +1,75 @@
+import { RootNode, NodeTypes } from "./ast";
+import { test, expect } from "vitest";
+import { transformer } from "./transformer";
+test("transformer", () => {
+  const originalAST: RootNode = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: NodeTypes.CallExpression,
+        name: "add",
+        params: [
+          {
+            type: NodeTypes.NumberLiteral,
+            value: "2",
+          },
+          {
+            type: NodeTypes.CallExpression,
+            name: "subtract",
+            params: [
+              {
+                type: NodeTypes.NumberLiteral,
+                value: "4",
+              },
+              {
+                type: NodeTypes.NumberLiteral,
+                value: "2",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  const transformedAST = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add",
+          },
+          arguments: [
+            {
+              type: "NumberLiteral",
+              value: "2",
+            },
+            {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "subtract",
+              },
+              arguments: [
+                {
+                  type: "NumberLiteral",
+                  value: "4",
+                },
+                {
+                  type: "NumberLiteral",
+                  value: "2",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  };
+
+  expect(transformer(originalAST)).toEqual(transformedAST);
+});
