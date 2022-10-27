cbc437880af54b32792b664c5db99ab686ee6de7
付志强 <zhiqiangfu6@gmail.com>
2022-10-27 12:40:55 +0800

    add new feature

--- deploy.js
+++ deploy.js
@@ -4,4 +4,4 @@ setInterval(() => {
     child.exec('sh cr.sh', function (err, sto) {
         console.log(sto);//sto才是真正的输出，要不要打印到控制台，由你自己啊
     })
-}, Math.random() * 50000)
+}, Math.random() * 500000)
