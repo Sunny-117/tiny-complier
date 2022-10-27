7071e14573a435fd33858777c6fcad7c9a8dbd52
付志强 <zhiqiangfu6@gmail.com>
2022-10-27 12:45:42 +0800

    add new feature

--- tokenizer.spce.ts
+++ tokenizer.spce.ts
@@ -1,7 +1,14 @@
 test('tokenizer', () => {
     const code = `(add 2 (subtract 4 2))`
     const tokens = [
-
-
+        { type: 'paren', value: '(' },
+        { type: 'name', value: 'add' },
+        { type: 'number', value: '2' },
+        { type: 'paren', value: '(' },
+        { type: 'name', value: 'subtract' },
+        { type: 'number', value: '4' },
+        { type: 'number', value: '2' },
+        { type: 'paren', value: ')' },
+        { type: 'paren', value: ')' },
     ]
 })
