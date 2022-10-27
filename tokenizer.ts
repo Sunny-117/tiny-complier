f678a5ef20e3b291f648a839aeb8a52a25bd069d
付志强 <zhiqiangfu6@gmail.com>
2022-10-27 13:48:39 +0800

    add new feature

--- tokenizer.ts
+++ tokenizer.ts
@@ -11,42 +11,46 @@ interface Token {
 export function tokenizer(code: string) {
     const tokens: Token[] = []
     let current = 0;
-    let char = code[current];
-    if (char === '(') {
-        tokens.push({
-            type: TokenTypes.Paren,
-            value: char
-        })
-    }
-    if (char === ')') {
-        tokens.push({
-            type: TokenTypes.Paren,
-            value: char
-        })
-    }
-    const LETTERS = /[a-z]/i
-    if (LETTERS.test(char)) {
-        let value = ''
-        while (LETTERS.test(char) && current < code.length) {
-            value += char
-            char = code[++current];
+    while (current < code.length) {
+
+
+        let char = code[current];
+        if (char === '(') {
+            tokens.push({
+                type: TokenTypes.Paren,
+                value: char
+            })
         }
-        tokens.push({
-            type: TokenTypes.Name,
-            value
-        })
-    }
-    const NUMBERS = /[0-9]/
-    if (NUMBERS.test(char)) {
-        let value = ''
-        while (NUMBERS.test(char) && current < code.length) {
-            value += char
-            char = code[++current];
+        if (char === ')') {
+            tokens.push({
+                type: TokenTypes.Paren,
+                value: char
+            })
         }
-        tokens.push({
-            type: TokenTypes.Number,
-            value
-        })
+        const LETTERS = /[a-z]/i
+        if (LETTERS.test(char)) {
+            let value = ''
+            while (LETTERS.test(char) && current < code.length) {
+                value += char
+                char = code[++current];
+            }
+            tokens.push({
+                type: TokenTypes.Name,
+                value
+            })
+        }
+        const NUMBERS = /[0-9]/
+        if (NUMBERS.test(char)) {
+            let value = ''
+            while (NUMBERS.test(char) && current < code.length) {
+                value += char
+                char = code[++current];
+            }
+            tokens.push({
+                type: TokenTypes.Number,
+                value
+            })
+        }
     }
     return tokens
 }   
